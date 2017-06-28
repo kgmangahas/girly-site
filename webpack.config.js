3aabc3c000647e5a88356d1a11e5ec4d00b3f1c3
Kate <kateline.mangahas@gmail.com>
2017-06-28 20:26:20 +0800

    Added initial base styles.

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+   
+
+    entry: { 
+      App: "./app/assets/scripts/App.js"
+    },
+    output: {
+        //change from path: "./app/temp/scripts" because of not an absolute path error
+        path:  __dirname + '/app/temp/scripts',
+        filename: "[name].js"
+    },
+    module: {
+        loaders: [
+            {
+                loader: 'babel-loader',
+                query: {
+                    presets: ['es2015']
+                },
+                test: /\.js$/,
+                exclude: /node_modules/
+            }
+        ]
+    }
+}
